f799da93db5eeadd9164df906e4d47bbe5b896a9
marconuss <marconuss95@gmail.com>
2020-01-08 20:14:53 +0100

    fixed dependencies
    fixed some dependecies with the examples for the serial port communication

--- SerialPortExample/Com.gd
+++ SerialPortExample/Com.gd
@@ -8,32 +8,32 @@ export (String) var endline= "\n"
 
 #byte size, to be used with open function (argument #4) and is optional
 enum  bytesz{
-    SER_BYTESZ_8 = 0,
-    SER_BYTESZ_7,
-    SER_BYTESZ_6,
-    SER_BYTESZ_5
+	SER_BYTESZ_8 = 0,
+	SER_BYTESZ_7,
+	SER_BYTESZ_6,
+	SER_BYTESZ_5
 }
 
 #parity, to be used with open function (argument #5) and is optional
 enum parity{
-    SER_PAR_NONE,
-    SER_PAR_ODD,
-    SER_PAR_EVEN,
-    SER_PAR_MARK,
-    SER_PAR_SPACE}
+	SER_PAR_NONE,
+	SER_PAR_ODD,
+	SER_PAR_EVEN,
+	SER_PAR_MARK,
+	SER_PAR_SPACE}
 
 #stopbyte, to be used with open function (argument #6) and is optional
 enum stopbyte{
-    SER_STOPB_ONE, #1
-    SER_STOPB_ONE5,#1.5
-    SER_STOPB_TWO} #2
+	SER_STOPB_ONE, #1
+	SER_STOPB_ONE5,#1.5
+	SER_STOPB_TWO} #2
 
 #queue, to be used with flush function 
 #if not added, the function defaults to SER_QUEUE_IN
 enum queue{
-    SER_QUEUE_IN,
-    SER_QUEUE_OUT,
-    SER_QUEUE_ALL}
+	SER_QUEUE_IN,
+	SER_QUEUE_OUT,
+	SER_QUEUE_ALL}
 
 
 #a readline function, just add the current Port (based on sercomm)
