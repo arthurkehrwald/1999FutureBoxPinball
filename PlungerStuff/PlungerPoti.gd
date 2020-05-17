b8451df5a5f0ea62a28ce55765e6a123abce926b
marconuss <marconuss95@gmail.com>
2020-05-17 19:48:35 +0200

    Added the plunger stuff, shouldn't affect anything...yet

---
+++ PlungerStuff/PlungerPoti.gd
@@ -0,0 +1,46 @@
+extends KinematicBody
+
+export var windup_speed = 3.0
+export var release_speed = 10.0
+export var max_distance = 5.0
+
+var start_pos = Vector3()
+var	max_pos = Vector3()
+var start_transform 
+var max_transform
+var move_progress = 0.0
+var is_at_start = true
+
+onready var pull_audio_player = get_node("PullAudioPlayer")
+onready var release_audio_player = get_node("ReleaseAudioPlayer")
+
+
+func _ready():
+	start_pos = get_translation()
+	max_pos = start_pos + get_transform().basis.z.normalized() * max_distance
+	start_transform = Transform(Basis.IDENTITY, start_pos)
+	max_transform = Transform(Basis.IDENTITY, max_pos)
+
+
+func _input(event):
+	if event.is_action_pressed("ui_down"):
+		pull_audio_player.play()
+	elif event.is_action_released("ui_down"):
+		release_audio_player.play()
+		pull_audio_player.stop()
+
+func _physics_process(delta):
+	#if Input.is_action_pressed("ui_down"):
+	#	if move_progress < 1:
+	#		move_progress += windup_speed / Engine.time_scale / max_distance * delta
+	#		pull_audio_player.pitch_scale = move_progress + .5
+	#elif move_progress > 0:
+	#	move_progress -= release_speed / Engine.time_scale / max_distance * delta
+	#	if move_progress < 0:
+	#		move_progress = 0
+	
+	#set_translation(start_pos.linear_interpolate(max_pos, move_progress))
+	set_transform(start_transform.interpolate_with(max_transform, move_progress))
+	
+func _on_PlungerReader_PlungerSignal(data):
+	move_progress = data
---
+++ PlungerStuff/SerialReader.cs
@@ -0,0 +1,94 @@
+using Godot;
+using System;
+using System.IO.Ports;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class SerialReader : Node
+{
+    public SerialPort _serialPort;
+    
+    [Export]
+    public string portName = "COM3";
+    [Export]
+    public int baudRate = 9600;
+
+
+    public string zString;
+    public float zFloat = 0.0f;
+    public float zNormalized = 0.0f;
+
+    [Signal]
+    public delegate void PlungerSignal();
+
+    public override void _Ready()
+    {
+        //System.Threading.Thread readThread = new System.Threading.Thread(Read);
+        //readThread.Start();
+        SetPhysicsProcess(false);
+
+        if(!(Array.Exists(SerialPort.GetPortNames(), element => element == portName)))
+        {
+            GD.Print("port not found");
+            return;
+        }
+        GD.Print("test");
+        _serialPort = new SerialPort();
+        _serialPort.PortName = portName;
+        _serialPort.BaudRate = baudRate;
+        _serialPort.DtrEnable = true;
+        _serialPort.RtsEnable = false;
+        _serialPort.Open();
+        SetPhysicsProcess(true);
+        GD.Print("port " + portName + " is open");   
+        //readThread.Start(); 
+        ReadData();
+    }
+
+/*
+    public void Read()
+    {
+        GD.Print("ReadThread started");
+        while(true)
+        {
+            //GD.Print("run");
+            zString = _serialPort.ReadLine();
+            
+                if(zString != null)
+                {
+                    zFloat = float.Parse(zString);
+                }
+
+                zNormalized = zFloat/1023;
+            //}
+            EmitSignal("PlungerSignal", zNormalized);
+            GD.Print(zNormalized);
+        }
+    }
+*/
+    public void ReadData()
+    {   
+        GD.Print("in the read data function");
+        Task radData = Task.Factory.StartNew(() =>
+        {
+            GD.Print("Task running");
+            while(true)
+            {
+
+            //if(_serialPort.IsOpen)
+            //{
+                zString = _serialPort.ReadLine();
+            
+                if(zString != null)
+                {
+                    zFloat = float.Parse(zString);
+                }
+
+                zNormalized = zFloat/1023;
+            //}
+            EmitSignal("PlungerSignal", zNormalized);
+            GD.Print(zNormalized);
+            }
+        });
+    }    
+}
