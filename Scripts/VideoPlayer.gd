d8057710c1bd5409f89bd1c3d1fb00a1bed1b2b2
arthurkehrwald <50906979+arthurkehrwald@users.noreply.github.com>
2020-05-17 19:13:12 +0200

    implemented stage transition overlays

--- Scripts/VideoPlayer.gd
+++ Scripts/VideoPlayer.gd
@@ -1,19 +1,39 @@
 extends VideoPlayer
 
+
+var video = {
+	GameState.TESTING: null,
+	GameState.PREGAME: preload("res://HUD/Videos/pregame_overlay.ogv"),
+	GameState.EXPOSITION: null,
+	GameState.ENEMY_FLEET: preload("res://HUD/Videos/stage_one_overlay.ogv"),
+	GameState.BOSS_APPEARS: preload("res://HUD/Videos/stage_two_overlay.ogv"),
+	GameState.MISSILES: null,
+	GameState.TREX: null,
+	GameState.BLACK_HOLE: null,
+	GameState.ECLIPSE: null,
+	GameState.VICTORY: null,
+	GameState.DEFEAT: preload("res://HUD/Videos/game_over_overlay.ogv")
+}
+
+
 func _ready():
 	GameState.connect("state_changed", self, "on_GameState_changed")
 	connect("finished", self, "on_finished")
 
 
 func on_GameState_changed(new_state, _is_debug_skip):
-	if new_state == GameState.DEFEAT:
-		set_visible(true)
+	if video[new_state] != null:
+		stream = video[new_state]
+		visible = true
+		get_tree().paused = true
 		play()
 	else:
-		set_visible(false)
+		visible = false
+		get_tree().paused = false
 		stop()
 
 
 func on_finished():
-	set_visible(false)
+	visible = false
+	get_tree().paused = false
 	GameState.handle_event(GameState.Event.POSTGAME_FINISHED)
