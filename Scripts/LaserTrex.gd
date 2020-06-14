8b91b91c2d1aabe5cec5f605f2ea1da30b566042
arthurkehrwald <50906979+arthurkehrwald@users.noreply.github.com>
2020-06-14 21:50:06 +0200

    laser t-rex sounds

--- Scripts/LaserTrex.gd
+++ Scripts/LaserTrex.gd
@@ -9,6 +9,8 @@ var time_until_laser_toggle = 0.0
 onready var health_bar = get_node("TrexBar3D/Viewport/TrexBar/HealthBar")
 onready var laser_area = get_node("LaserArea")
 onready var laser_toggle_timer = get_node("LaserToggleTimer")
+onready var growl_audio_player = get_node("GrowlAudioPlayer")
+onready var laser_audio_player = get_node("LaserAudioPlayer")
 
 
 func _enter_tree():
@@ -43,6 +45,11 @@ func set_laser_is_active(value):
 	laser_area.set_visible(value)
 	laser_toggle_timer.start(LASER_UPTIME if value else LASER_DOWNTIME)
 	laser_is_active = value
+	if value:
+		laser_audio_player.play()
+		growl_audio_player.play()
+	else:
+		laser_audio_player.stop()
 
 
 func on_LaserToggleTimer_timeout():
