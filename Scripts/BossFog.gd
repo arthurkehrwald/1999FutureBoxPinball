3976d48bec70ce8e993afce0c240f87f3c1e9970
arthurkehrwald <50906979+arthurkehrwald@users.noreply.github.com>
2022-06-09 17:44:50 +0200

    Reintroduce boss fog

--- Scripts/BossFog.gd
+++ Scripts/BossFog.gd
@@ -1,16 +1,10 @@
 extends "res://Scripts/ToggleParticles.gd"
 
-onready var boss = get_node("..")
 onready var mesh = get_node("MeshInstance3")
 onready var col_layer_when_enabled = call("get_collision_layer")
 
 
-func _ready():
-	if boss is Boss:
-		boss.connect("is_vulnerable_changed", self, "on_Boss_is_vulnerable_changed")
-
-
-func on_Boss_is_vulnerable_changed(value):
+func _on_Boss_is_vulnerable_changed(value):
 	toggle(!value)
 	mesh.visible = !value
 	call("set_collision_layer", 0 if value else col_layer_when_enabled)
