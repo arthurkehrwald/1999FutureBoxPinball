18bd3e9558a85e5d4d6686272cdc902dbc06230d
Arthur <arthur.kehrwald@gmail.com>
2022-07-23 14:51:37 +0200

    Fog doesn't show up when boss dies

--- Scripts/BossFog.gd
+++ Scripts/BossFog.gd
@@ -3,8 +3,24 @@ extends "res://Scripts/ToggleParticles.gd"
 onready var mesh = get_node("MeshInstance3")
 onready var col_layer_when_enabled = call("get_collision_layer")
 
+var is_boss_vulnerable = false
+var is_boss_dead = false
 
 func _on_Boss_is_vulnerable_changed(value):
-	toggle(!value)
-	mesh.visible = !value
-	call("set_collision_layer", 0 if value else col_layer_when_enabled)
+	is_boss_vulnerable = value
+	evaluate()
+
+
+func _on_Boss_health_changed(health, old_health, max_health):
+	is_boss_dead = health <= 0
+	evaluate()
+
+
+func evaluate():
+	toggle(!is_boss_vulnerable and !is_boss_dead)
+
+
+func toggle(enable):
+	.toggle(enable)
+	mesh.visible = enable
+	call("set_collision_layer", col_layer_when_enabled if enable else 0)
