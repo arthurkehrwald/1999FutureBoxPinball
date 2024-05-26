4fccb59d1f1047d15e182f85efc59b803c29a8cf
arthurkehrwald <50906979+arthurkehrwald@users.noreply.github.com>
2024-05-26 19:01:40 +0200

    Toggle highscore tracking with Ctrl H

---
+++ Scripts/HighscoreTrackingIndicator.gd
@@ -0,0 +1,12 @@
+extends CanvasItem
+
+
+func _ready() -> void:
+	if Globals.highscore_tracker != null:
+		visible = Globals.highscore_tracker.get_is_tracking()
+		Globals.highscore_tracker.connect("highscore_tracking_toggled", self, "_on_HighscoreTracker_highscore_tracking_toggled")
+	else:
+		visible = false
+
+func _on_HighscoreTracker_highscore_tracking_toggled(is_tracking: bool) -> void:
+	visible = is_tracking
