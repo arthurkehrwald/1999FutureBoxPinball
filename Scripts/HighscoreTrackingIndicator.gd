6b36814202d2ad24c1d29c1651e3f0b513eea087
arthurkehrwald <50906979+arthurkehrwald@users.noreply.github.com>
2025-04-22 17:01:06 +0200

    Invert highscore tracking indicator

--- Scripts/HighscoreTrackingIndicator.gd
+++ Scripts/HighscoreTrackingIndicator.gd
@@ -3,10 +3,10 @@ extends CanvasItem
 
 func _ready() -> void:
 	if Globals.highscore_tracker != null:
-		visible = Globals.highscore_tracker.get_is_tracking()
+		visible = !Globals.highscore_tracker.get_is_tracking()
 		Globals.highscore_tracker.connect("highscore_tracking_toggled", self, "_on_HighscoreTracker_highscore_tracking_toggled")
 	else:
-		visible = false
+		visible = true
 
 func _on_HighscoreTracker_highscore_tracking_toggled(is_tracking: bool) -> void:
-	visible = is_tracking
+	visible = !is_tracking
